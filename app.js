6507550a774e9fe5e644e506a686f3049891bcab
Alex-Andrer <alexandreizuku@gmail.com>
2023-11-05 19:33:54 -0300

    sistema de validação dos campos

--- app.js
+++ app.js
@@ -1,31 +1,80 @@
 const eAnoBissexto = (ano) => {
   return (ano % 4 === 0 && ano % 100 !== 0) || (ano % 400 === 0);
 }
-
-document.getElementById("botaoCalcularIdade").addEventListener("click", function () {
-  calculaIdade();
+//ação do botão
+document.getElementById("botaoCalcularIdade").addEventListener("click",() => {
+  validaIdade();
 });
 
+//sistema de validação dos campos
+const validaIdade = () => {
 
+  const dia = document.getElementById("dia").value;
+  const mes = document.getElementById("mes").value;
+  const ano = document.getElementById("ano").value;
 
+  const erroDia = document.getElementById("erroNoDia");
+  const erroMes = document.getElementById("erroNoMes");
+  const erroAno = document.getElementById("erroNoAno");
+
+  let temErro = false;
+
+  if(dia === ""){
+    erroDia.textContent = "Preencha o dia.";
+    erroDia.style.display = "block";
+    document.getElementById("dia").style.borderColor = "red";
+    temErro = true;
+  }else{
+    erroDia.style.display = "none";
+    document.getElementById("dia").style.borderColor = "";
+  }
+
+  if(mes === ""){
+    erroMes.textContent = "Preencha o mês.";
+    erroMes.style.display = "block";
+    document.getElementById("mes").style.borderColor = "red";
+    temErro = true;
+  }else{
+    erroMes.style.display = "none";
+    document.getElementById("mes").style.borderColor = "";
+  }
+
+  if(ano === ""){
+    erroAno.textContent = "Preencha o ano.";
+    erroAno.style.display = "block";
+    document.getElementById("ano").style.borderColor = "red";
+    temErro = true;
+  }else{
+    erroAno.style.display = "none";
+    document.getElementById("ano").style.borderColor = "";
+  }
+
+  if(temErro){
+    return;
+  }
+
+  calculaIdade();
+
+}
+//cálculo da idade
 const calculaIdade = () => {
   //Pega valores
-  const diaDoNascimento = parseInt(document.getElementById("dia").value);
-  const mesDoNascimento = parseInt(document.getElementById("mes").value);
-  const anoDoNascimento = parseInt(document.getElementById("ano").value);
+  let diaDoNascimento = parseInt(document.getElementById("dia").value);
+  let mesDoNascimento = parseInt(document.getElementById("mes").value);
+  let anoDoNascimento = parseInt(document.getElementById("ano").value);
 
 
   //cálculo de data
-  const dataAtual = new Date();
-  const dataDeNascimento = new Date(anoDoNascimento, mesDoNascimento - 1, diaDoNascimento);
+  let dataAtual = new Date();
+  let dataDeNascimento = new Date(anoDoNascimento, mesDoNascimento - 1, diaDoNascimento);
 
-  const idadeEmSegundos = dataAtual - dataDeNascimento;
+  let idadeEmSegundos = dataAtual - dataDeNascimento;
 
-  const idadeData = new Date(idadeEmSegundos);
-  const ano = idadeData.getUTCFullYear() - 1970;
+  let idadeData = new Date(idadeEmSegundos);
+  let ano = idadeData.getUTCFullYear() - 1970;
 
-  const anoEBissexto = eAnoBissexto(ano);
-  const anoAtualEBissexto = eAnoBissexto = eAnoBissexto(dataAtual.getUTCFullYear());
+  let anoEBissexto = eAnoBissexto(ano);
+  let anoAtualEBissexto = eAnoBissexto(dataAtual.getUTCFullYear());
 
   if (mes > 2 && anoEBissexto) {
     ano++;
@@ -35,12 +84,12 @@ const calculaIdade = () => {
     ano++;
   };
 
-  const meses = idadeData.getUTCMonth();
-  const dia = idadeData.getUTCDay();
+  let meses = idadeData.getUTCMonth();
+  let dia = idadeData.getUTCDate();
 
 
   //mostra na tela
-  const resultado = `${ano} anos <br>${meses} meses <br>${dia} dias`;
+  let resultado = `${ano} anos <br>${meses} meses <br>${dia} dias`;
 
   document.getElementById("resultado").innerHTML = resultado;
 }
